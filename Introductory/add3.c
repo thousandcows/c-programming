108642d71a5527e73b9a3250ea9113d34f523f39
HYUNWOO CHEON <milcamboo@gmail.com>
2023-04-18 22:14:16 +0900

    c-introductory

---
+++ Introductory/add3.c
@@ -0,0 +1,14 @@
+/* Read in Three floats and Print Sum */
+
+#include <stdio.h>
+
+int main(void)
+{
+    float a, b, c, sum;
+    printf("Input three floats:");
+    scanf("%f%f%f", &a, &b, &c);
+    printf("a = %f, b = %f, c = %f\n", a, b, c);
+    sum = a + b + c;
+    printf("sum = %f\n\n", sum);
+    return 0;
+}
